55138a7f7af1ce0cecae8321fd6926954a99cf6b
Dzecsid <pro1oomisha@mail.ru>
2021-11-20 21:32:58 +0500

    12

--- filter.py
+++ filter.py
@@ -2,7 +2,6 @@ from PIL import Image
 import numpy as np
 import doctest
 
-
 def get_average(arr, mosaic_size):
     """
     Возвращает среднее значение сумм цветов каждого пикселя на участве arr с шагом в mosaic_size
@@ -23,7 +22,6 @@ def get_average(arr, mosaic_size):
 
     return int(np.sum(arr) / 3 // (mosaic_size ** 2))
 
-
 def draw_mosaic_pixel(color):
     """
     Рисует мозаику поверх старого изображения
@@ -37,34 +35,26 @@ def draw_mosaic_pixel(color):
         for b in range(j, j + mosaic_size):
             img_array[a][b] = [color, color, color]
 
-
 print(doctest.testmod())
-
 print('Введите название загружаемого изображения:')
 input_image_name = input()
-
 img = Image.open(input_image_name)
 img_array = np.array(img)
 width, height = img.size
-
 print('Введите размер мозаики: ')
 mosaic_size = int(input())
 print('Введите шаг серого: ')
 graduation = int(input())
-
 # растягиваю или сжимаю изображение, чтобы оно соответсвовало целому числу пикселей мозайки
 if width % mosaic_size != 0 or height % mosaic_size != 0:
     width = round(width / mosaic_size) * mosaic_size
     height = round(height / mosaic_size) * mosaic_size
     img_array = np.array(img.resize((width, height)))
-
 for i in range(0, height, mosaic_size):
     for j in range(0, width, mosaic_size):
         average = get_average(img_array[i:i + mosaic_size, j:j + mosaic_size], mosaic_size)
         draw_mosaic_pixel(int(average // graduation) * graduation)
-
 print('Введите название выгружаемого изображения:')
 output_image_name = input()
-
 res = Image.fromarray(img_array)
 res.save(output_image_name)
